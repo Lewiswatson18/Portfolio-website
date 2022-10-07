672421dff72ccc2bdce793de7c475b612730655d
Lewiswatson18 <lewiswatson@live.co.uk>
2022-10-07 15:34:19 +0100

    Add About Section

---
+++ src/components/About.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+
+
+
+            <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+
+                <div className=' pb-8 '>
+
+                    <p className=' text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+
+                </div>
+
+
+                <p className='text-xl mt-20  '>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur quas aperiam modi, optio accusamus enim fuga quo quis sapiente nisi hic ut assumenda animi temporibus nobis iusto nostrum doloremque. Aspernatur exercitationem dignissimos nulla omnis illo sint, laudantium eaque libero neque ratione sapiente eum, cumque odit minima atque. Voluptatum, cum eum.</p>
+                <br />
+
+                <p className=' text-xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A laborum assumenda mollitia, rerum eum, reiciendis vel odit reprehenderit ea ratione ipsum laudantium aliquid nulla maiores dolorem dolores quia quas corporis! Ea laboriosam accusamus consectetur! Illum voluptas consequuntur quis, quod ab hic in? Odit obcaecati quibusdam labore et quisquam eligendi voluptate.</p>
+            </div>
+
+
+
+
+        </div>
+    )
+}
+
+export default About
